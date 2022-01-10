8ed6206cbe3bca70912b4bd2dc8a72ed0d7d9ce5
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-11 00:27:51 +0100

    feat: new type NameList

--- app/src/types.ts
+++ app/src/types.ts
@@ -3,3 +3,4 @@ import type { ViteSSGContext } from 'vite-ssg'
 export type UserModule = (ctx: ViteSSGContext) => void
 export type Dictionary<T> = { [index: string]: T }
 export type OccurrenceTable = Dictionary<Dictionary<number>>
+export type NameList = Array<string>
