6f6fd7514198f3789b7a56d7d501c511f65ba1b7
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-11 00:27:23 +0100

    feat: new type OccurenceTable

--- app/src/types.ts
+++ app/src/types.ts
@@ -2,3 +2,4 @@ import type { ViteSSGContext } from 'vite-ssg'
 
 export type UserModule = (ctx: ViteSSGContext) => void
 export type Dictionary<T> = { [index: string]: T }
+export type OccurrenceTable = Dictionary<Dictionary<number>>
