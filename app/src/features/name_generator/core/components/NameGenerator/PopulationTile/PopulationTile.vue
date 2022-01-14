18896968ddac4f4259efec7a0d4ed2bd142a0175
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-14 17:35:54 +0100

    feat: (wip) start implementing population tiles

--- app/src/features/name_generator/core/components/NameGenerator/PopulationTile/PopulationTile.vue
+++ app/src/features/name_generator/core/components/NameGenerator/PopulationTile/PopulationTile.vue
@@ -1,3 +1,28 @@
+<script setup lang="ts">
+  import type { populationData } from '@API/local_placeholder'
+  import { AGGLOMERATIONS, GENDERS } from '@API/local_placeholder'
+
+  defineProps<{
+    isCarouselMoving: Boolean
+    populationName: String
+    populationData: populationData
+    generationTriggerCallback: Function
+  }>()
+</script>
+
 <template>
-  <p>HHH</p>
+  <div>
+    <div>HEADER</div>
+    <div>
+      <div v-for="([critName, critValues], index) in Object.entries(populationData.criteria)" :key="index">
+        <button
+          v-for="(critValue, index2) in critValues"
+          :key="index2"
+          @click.stop="generationTriggerCallback(populationName, critName, critValue)"
+        >
+          {{ GENDERS[critValue] || AGGLOMERATIONS[critValue] }}
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
