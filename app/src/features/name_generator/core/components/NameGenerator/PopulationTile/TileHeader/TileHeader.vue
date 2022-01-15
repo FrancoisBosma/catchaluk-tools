5528476a7e4b6841c1e80f15b3fde1745b7e5c22
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-15 19:01:21 +0100

    feat: implement empty info button, missing its content

--- app/src/features/name_generator/core/components/NameGenerator/PopulationTile/TileHeader/TileHeader.vue
+++ app/src/features/name_generator/core/components/NameGenerator/PopulationTile/TileHeader/TileHeader.vue
@@ -11,32 +11,47 @@
 </script>
 
 <template>
-  <h3 class="title flex flex-auto justify-center items-center">
-    <a
-      v-if="typeof redirectionLinks.population === 'string'"
-      :href="redirectionLinks.population"
-      target="_blank"
-      rel="noopener noreferrer"
-      class="ext-link"
-    >
-      <span class="title relative">
-        {{ populationName }}<uil-external-link-alt class="ext-link-icon absolute top-[-45%]" />
-      </span>
-    </a>
-    <template v-else>
-      <a :href="redirectionLinks.population[0]" target="_blank" rel="noopener noreferrer" class="ext-link">
+  <div w:position="relative" class="flex flex-auto justify-center items-center">
+    <h3>
+      <a
+        v-if="typeof redirectionLinks.population === 'string'"
+        :href="redirectionLinks.population"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="ext-link"
+      >
         <span class="title relative">
-          {{ populationName.split('&')[0] }}<uil-external-link-alt class="ext-link-icon absolute top-[-45%]" />
+          {{ populationName }}<uil-external-link-alt class="ext-link-icon absolute top-[-45%]" />
         </span>
       </a>
-      <span class="title">&nbsp; et&nbsp;</span>
-      <a :href="redirectionLinks.population[1]" target="_blank" rel="noopener noreferrer" class="ext-link">
-        <span class="title relative">
-          {{ populationName.split('&')[1] }}<uil-external-link-alt class="ext-link-icon absolute top-[-45%]" /> &nbsp;
+      <template v-else>
+        <a :href="redirectionLinks.population[0]" target="_blank" rel="noopener noreferrer" class="ext-link">
+          <span class="title relative">
+            {{ populationName.split('&')[0] }}<uil-external-link-alt class="ext-link-icon absolute top-[-45%]" />
+          </span>
+        </a>
+        <span class="title">&nbsp; et&nbsp;</span>
+        <a :href="redirectionLinks.population[1]" target="_blank" rel="noopener noreferrer" class="ext-link">
+          <span class="title relative">
+            {{ populationName.split('&')[1] }}<uil-external-link-alt class="ext-link-icon absolute top-[-45%]" /> &nbsp;
+          </span>
+        </a>
+      </template>
+    </h3>
+    <div v-if="redirectionLinks.info" class="absolute top-0 right-0">
+      <Popper placement="top" arrow class="absolute top-[22px] right-0">
+        <span w:cursor="pointer">
+          <fluent-question-48-filled
+            w:bg="[aliceblue]"
+            w:border="2 [var(--red)] rounded-full"
+            w:p="[2px]"
+            w:text="xl"
+            style="box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.5)"
+          />
         </span>
-      </a>
-    </template>
-  </h3>
+      </Popper>
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- app/src/generated_global_components.d.ts
+++ app/src/generated_global_components.d.ts
@@ -4,6 +4,7 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
+    FluentQuestion48Filled: typeof import('~icons/fluent/question48-filled')['default']
     FontelicoEmoSunglasses: typeof import('~icons/fontelico/emo-sunglasses')['default']
     Footer: typeof import('./global/components/Footer.vue')['default']
     UilExternalLinkAlt: typeof import('~icons/uil/external-link-alt')['default']
