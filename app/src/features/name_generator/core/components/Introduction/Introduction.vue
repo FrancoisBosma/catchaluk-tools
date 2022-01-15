480c73692ec0f4699bf02be2936da35a66536f5b
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-15 17:50:44 +0100

    refactor: add missing redirection to link

--- app/src/features/name_generator/core/components/Introduction/Introduction.vue
+++ app/src/features/name_generator/core/components/Introduction/Introduction.vue
@@ -1,7 +1,9 @@
 <template>
   <h1>Catchaluk tools</h1>
   <h3>
-    <a href="" class="ext-link"> Catchaluk<uil-external-link-alt class="ext-link-icon" /></a>
+    <a href="https://catchaluk.wixsite.com/lejdr" target="_blank" rel="noopener noreferrer" class="ext-link">
+      Catchaluk<uil-external-link-alt class="ext-link-icon"
+    /></a>
     <span>
       est un univers de jeu de rôle inspiré de l'Amérique centrale pré-colombienne dans lequel les joueurs sont plongés
       dans une société atypique; incarnant des personnages normaux tout aussi mortels que les autres; où diplomatie,
