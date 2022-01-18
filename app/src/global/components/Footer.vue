3be42f1f77986d57b27bd5b94bcf27edfcec3e1a
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-18 18:30:19 +0100

    feat: implment Foote's content

--- app/src/global/components/Footer.vue
+++ app/src/global/components/Footer.vue
@@ -1,7 +1,22 @@
 <template>
-  <footer class="bg-gray-700 py-4 text-gray-100 dark:(text-gray-700 bg-gray-200)">
-    <div class="mt-5 mx-auto text-center opacity-50 text-sm">
-      [ Implemented by Francois Bosma <fontelico-emo-sunglasses /> ]
-    </div>
+  <footer w:p="8" w:text="[#eee]" class="grid grid-rows-2 grid-cols-[1fr,auto,1fr] gap-2">
+    <span w:text="right">Implemented with</span>
+    <span><ion-md-heart class="icon" /></span>
+    <span>by Francois Bosma</span>
+    <a href="https://github.com/FrancoisBosma" target="_blank" rel="noopener noreferrer" w:text="right">
+      <fontelico-emo-sunglasses class="icon-btn" />
+    </a>
+    <a href="https://github.com/FrancoisBosma" target="_blank" rel="noopener noreferrer">
+      <zmdi-github-alt class="icon-btn animate-delay-75" />
+    </a>
+    <a href="https://linkedin.com/in/francois-bosma-1051849b/" target="_blank" rel="noopener noreferrer"
+      ><fontisto-linkedin class="icon-btn animate-delay-150"
+    /></a>
   </footer>
 </template>
+
+<style>
+  footer * {
+    font-family: 'Alliance No.1';
+  }
+</style>
