cdf3fa2da3e69ec12611b68ee49896b3f7ed41b1
FrancoisBosma <francois.bosma@hotmail.fr>
2022-01-19 14:33:45 +0100

    style: make link bounce rather than inner icon

--- app/src/global/components/Footer.vue
+++ app/src/global/components/Footer.vue
@@ -1,17 +1,26 @@
 <template>
-  <footer w:p="8" w:text="[#eee]" class="grid grid-rows-2 grid-cols-[1fr,auto,1fr] gap-2">
-    <span w:text="right">Implemented with</span>
-    <span><ion-md-heart class="icon" /></span>
-    <span>by Francois Bosma</span>
-    <a href="https://github.com/FrancoisBosma" target="_blank" rel="noopener noreferrer" w:text="right">
-      <fontelico-emo-sunglasses class="icon-btn" />
-    </a>
-    <a href="https://github.com/FrancoisBosma" target="_blank" rel="noopener noreferrer">
-      <zmdi-github-alt class="icon-btn animate-delay-75" />
-    </a>
-    <a href="https://linkedin.com/in/francois-bosma-1051849b/" target="_blank" rel="noopener noreferrer"
-      ><fontisto-linkedin class="icon-btn animate-delay-150"
-    /></a>
+  <footer w:p="8" w:text="[#eee]" w:flex="~ col" w:align="items-center" w:gap="2">
+    <div class="grid grid-rows-1 grid-cols-[1fr,auto,1fr] gap-1">
+      <span w:text="right">Implemented with</span>
+      <span><ion-md-heart class="icon" /></span>
+      <span>by Francois Bosma</span>
+    </div>
+    <div class="grid grid-rows-1 grid-cols-[1fr,auto,1fr] gap-3 w-max text-center">
+      <a href="" target="_blank" rel="noopener noreferrer" w:animate="bounce">
+        <fontelico-emo-sunglasses class="icon-btn" />
+      </a>
+      <a href="https://github.com/FrancoisBosma" target="_blank" rel="noopener noreferrer" w:animate="bounce delay-75">
+        <zmdi-github-alt class="icon-btn" />
+      </a>
+      <a
+        href="https://linkedin.com/in/francois-bosma-1051849b/"
+        target="_blank"
+        rel="noopener noreferrer"
+        w:animate="bounce delay-150"
+      >
+        <fontisto-linkedin class="icon-btn" />
+      </a>
+    </div>
   </footer>
 </template>
 
